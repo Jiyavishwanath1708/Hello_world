a0ca4c3c4f7fe0159528b900e5d4bdffb9a9f9e7
Jiya Vishwanath <jiyavishwanath80@gmail.com>
2026-03-27 19:15:56 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,25 @@
 #include <stdio.h> 
 int main()
     {
-     printf("Hello, World!");
-      return 0; 
+      printf("\n");
+     printf("\nHello, World!\n");
+    
+    printf("\n");
+//history of c programming in short. 
+
+    printf("-------------------------------------------------------------\n");
+    printf("| Year      | Event                         | Creator        |\n");
+    printf("-------------------------------------------------------------\n");
+    printf("| 1960s     | ALGOL Language Developed     | ALGOL Committee|\n");
+    printf("| 1967      | BCPL Language Developed      | Martin Richards|\n");
+    printf("| 1970      | B Language Developed         | Ken Thompson   |\n");
+    printf("| 1972      | C Language Developed         | Dennis Ritchie |\n");
+    printf("| 1978      | C Book Published             | Kernighan & Ritchie |\n");
+    printf("| 1989      | ANSI C Standard              | ANSI Committee |\n");
+    printf("| 1999      | C99 Standard                 | ISO            |\n");
+    printf("| 2011      | C11 Standard                 | ISO            |\n");
+    printf("| 2018      | C18 Standard                 | ISO            |\n");
+    printf("-------------------------------------------------------------\n");
+
+    return 0;
     }
